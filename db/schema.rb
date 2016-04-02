22617c0502668bed5f4db7b24528951b2e44326a
andre73dev <andre73.dev@gmail.com>
2016-04-02 13:52:38 +0000

    fix_model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329130126) do
+ActiveRecord::Schema.define(version: 20160402134625) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
